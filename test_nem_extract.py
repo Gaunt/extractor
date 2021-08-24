f195f240cde25384d9e8ee790e47cdeed7e2e0ed
Karel Novak <novakk5@gmail.com>
2021-08-24 23:48:57 +0200

    Revert "fix msgpackencoding"
    This reverts commit 878565f54113f72784e3975f6fe076326eb4aa68.

--- test_nem_extract.py
+++
@@ -1,6 +0,0 @@
-import nem_extract
-
-
-def test_stm_file():
-    statements = nem_extract.load_stm_data()
-    assert list(statements.keys()) == ['transaction_statements', 'address_resolution_statements', 'mosaic_resolution_statements']
