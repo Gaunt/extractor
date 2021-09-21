e2ef64e42710e293f3b560679bcc84d9067ab9e2
Karel Novak <novakk5@gmail.com>
2021-09-22 00:39:50 +0200

    remove test_db.py

--- test_nem_extract.py
+++ test_nem_extract.py
@@ -1,28 +1,14 @@
-import pytest
-import subprocess
-import pathlib
-import shlex
 import nem_extract
 import state
 
 
-@pytest.fixture(scope="session", autouse=True)
-def fetch_text_data_dir():
-    p = pathlib.Path("./symbol_test_data")
-    if not p.exists():
-        subprocess.run(
-            shlex.split(
-                """git clone https://github.com/Gaunt/symbol_test_data"""
-            )
-        )
-
-
-def test_state_map(tmp_path):
+def test_main(tmp_path, data_dir):
+    data_main = str(data_dir / 'data_main')
     state_map_path = str(tmp_path / "./state_map.msgpack")
     nem_extract.main(
         nem_extract.parse_args(
             [
-                "--block_dir=./symbol_test_data/data_main",
+                f"--block_dir={str(data_dir)}",
                 f"--state_save_path={state_map_path}",
                 "--quiet",
             ]
