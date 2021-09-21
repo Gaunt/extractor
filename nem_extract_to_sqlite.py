e60e15279a881aa76c576050255915ae52e302e8
Karel Novak <novakk5@gmail.com>
2021-09-21 21:00:08 +0200

    to save to sql

--- nem_extract_to_sqlite.py
+++ nem_extract_to_sqlite.py
@@ -5,7 +5,7 @@ import operator as op
 import sys
 import nem_extract
 from tqdm import tqdm
-import db
+import sql
 
 
 def parse_args(argv):
@@ -19,7 +19,7 @@ def parse_args(argv):
     parser.add_argument(
         "--sqlite_db_file",
         type=str,
-        default=db.DB_FILE,
+        default=sql.DB_FILE,
         help="path to write the extracted block data to",
     )
     parser.add_argument(
@@ -41,69 +41,32 @@ def parse_args(argv):
     return args
 
 
-def decode_hashes(block):
-    hashes = block["tx_hashes"]
-    if not hashes:
-        yield from it.repeat(
-            (
-                None,
-                None,
-            )
-        )
-    else:
-        for tx_hashes in hashes:
-            yield tx_hashes["entity_hash"].hex().upper(), tx_hashes[
-                "merkle_component_hash"
-            ].hex().upper()
-
-
 def main(args):
     if args.quiet:
         nem_extract.tqdm = functools.partial(tqdm, disable=True)
 
-    db.db_clean()
-    db.db_init()
+    sql.db_clean()
+    sql.db_init()
 
-    with db.batch_saver() as saver:
-        block_paths = nem_extract.get_block_paths(
-            args.block_dir, args.block_extension
-        )
-        blocks = nem_extract.deserialize_blocks(block_paths, True)
-        state_map = nem_extract.XYMStateMap()
+    block_paths = nem_extract.get_block_paths(
+        args.block_dir, args.block_extension
+    )
+    blocks = nem_extract.deserialize_blocks(block_paths, True)
+    sql.insert_blocks(blocks)
 
-        for block in blocks:
-            height = block["header"]["height"]
-            transactions = block["footer"]["transactions"]
-            tx_hashes = decode_hashes(block)
-            for tx, (entity_hash, merkle_component_hash) in zip(
-                transactions, tx_hashes
-            ):
-                saver(
-                    state_map.decompose_tx(
-                        tx,
-                        height,
-                        block["header"]["fee_multiplier"],
-                        tx_hash=entity_hash,
-                    ),
-                )
+    print("block data extraction complete!\n")
+    print(f"block data written to {args.sqlite_db_file}")
 
-        print("block data extraction complete!\n")
-        print(f"block data written to {args.sqlite_db_file}")
+    statement_paths = nem_extract.get_statement_paths(
+        block_dir=args.block_dir,
+        statement_extension=args.statement_extension,
+    )
 
-        statements = nem_extract.deserialize_statements(
-            nem_extract.get_statement_paths(
-                block_dir=args.block_dir,
-                statement_extension=args.statement_extension,
-            )
-        )
+    statements = nem_extract.deserialize_statements(statement_paths)
+    sql.insert_statements(statements)
 
-        for height, stmts, s_path in statements:
-            for stmt in stmts["transaction_statements"]:
-                for rx in stmt["receipts"]:
-                    saver(state_map.decompose_rx(rx, height))
-
-        print("statement data extraction complete!\n")
-        print(f"statement data written to {args.sqlite_db_file}")
+    print("statement data extraction complete!\n")
+    print(f"statement data written to {args.sqlite_db_file}")
 
 
 if __name__ == "__main__":
